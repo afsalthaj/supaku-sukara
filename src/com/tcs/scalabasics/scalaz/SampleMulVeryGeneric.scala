788fe5ccb19b47dcc71927684f28d7f5f5e0b17b
afsal <afsal.thaj@tcs.com>
2014-07-07 17:06:21 +0530

    committing the basics of scala polymorphism, implicit parameters etc
    Change-Id: I5f83fb5e94e50d226d6db5c611387d71ce6bc6ee

---
+++ src/com/tcs/scalabasics/scalaz/SampleMulVeryGeneric.scala
@@ -0,0 +1,22 @@
+package com.tcs.scalabasics.scalaz
+
+
+trait GenericMultiplier[A]{
+  
+  def mul(a:A,b:A) :A
+  
+}
+object SampleMulVeryGeneric  {
+  
+  implicit val multiplier=MultiplierInt
+
+   def main(args: Array[String]) {
+   mul(1,2)
+}
+  def mul[A](a:A,b:A)(implicit gm: GenericMultiplier[A])=gm.mul(a, b)
+  
+}
+
+object MultiplierInt extends GenericMultiplier[Int]{
+  def mul(a:Int,b:Int) :Int =a
+}
