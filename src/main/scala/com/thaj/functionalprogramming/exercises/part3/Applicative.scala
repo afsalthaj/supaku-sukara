ef9b40541ec88e1046aa47e6acfe2cf896cd6327
afsal.thaj <afsal.taj06@gmail.com>
2017-04-16 20:12:17 +1000

    more exercises on applicative

--- src/main/scala/com/thaj/functionalprogramming/exercises/part3/Applicative.scala
+++ src/main/scala/com/thaj/functionalprogramming/exercises/part3/Applicative.scala
@@ -34,6 +34,41 @@ object Applicative {
       * Note that the implementation of traverse is unchanged.
       * We can similarly move other combinators into Applicative that don’t depend directly on flatMap or join
       */
+    // Exercise 12.1
     def sequence[A](lfa: List[F[A]]): F[List[A]] = traverse(lfa)(fa => fa)
+    def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
+    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((_, _))
+  }
+
+  // Hard: The name applicative comes from the fact that we can formulate the Applicative
+  // interface using an alternate set of primitives, unit and the function apply, rather than unit and map2.
+  // Show that this formulation is equivalent in expressiveness by defining map2 and map in terms of unit and apply.
+  // Also establish that apply can be implemented in terms of map2 and unit.
+
+  // EXERCISE 12.2
+  trait RealApplicative[F[_]] extends Functor[F] {
+    def apply[A,B](fab: F[A => B])(fa: F[A]): F[B]
+    def unit[A](a: => A): F[A]
+
+
+    def map[A, B](fa: F[A])(f: A => B): F[B] = apply(unit[A => B](f))(fa)
+
+
+    def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = apply(apply(unit[A => B => C](f.curried))(fa))(fb)
+
+    def applyUsingMap[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fa, fab)((a, ab) => ab(a))
+
+    // Exercise 12.3
+    // The apply method is useful for implementing map3, map4, and so on, and the pattern is straightforward.
+    // Implement map3 and map4 using only unit, apply, and the curried method available on functions
+    def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] = {
+      val curriedBox: F[A => (B => C => D)] = unit(f.curried)
+      apply(apply(apply(curriedBox)(fa))(fb))(fc)
+    }
+
+    def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] = {
+      val curriedBox: F[A => (B => C => D => E)] = unit(f.curried)
+      apply(apply(apply(apply(curriedBox)(fa))(fb))(fc))(fd)
+    }
   }
 }
