0610a860308614fbaed9e09baa96d011e29aeb44
afsal.thaj <afsal.taj06@gmail.com>
2017-05-06 18:50:02 +1000

    cats basics

--- src/main/scala/com/thaj/functionalprogramming/cats/CatsOperations.scala
+++ src/main/scala/com/thaj/functionalprogramming/cats/CatsOperations.scala
@@ -1,11 +1,37 @@
 package com.thaj.functionalprogramming.cats
 
+import cats.Semigroup
+
+import cats.instances.all._
 /**
   * Created by afsalthaj on 6/05/2017.
   */
-class CatsOperations {
+// http://typelevel.org/cats/typeclasses.html
+// http://typelevel.org/cats/typeclasses/semigroup.html
+// An example of semigroup without using simulacrum, and with cats implicit Semigroup for Map[String, Int]
+object CatsOperations {
+  object OpsOperations {
 
-  // def combineAll[A <: Monoid[A]](list: List[A]): A = ???
+    implicit val semigroupMap = Semigroup[Map[String, Int]]
 
+    // good to have a sense of Ops trait
+    trait Ops[A] {
+      def self: A
 
+      def typeClassInstance: Semigroup[A]
+
+      def |++|(that: A): A = typeClassInstance.combine(self, that)
+    }
+
+    object ops {
+      implicit def toConvertOpsInstance[A](x: A)(implicit m: Semigroup[A]) = new Ops[A] {
+        def self = x
+
+        override def typeClassInstance: Semigroup[A] = Semigroup[A]
+      }
+    }
+  }
+
+  import OpsOperations.ops._
+  val s = Map("afsal" -> 2) |++| Map ("thaj" -> 2)
 }
